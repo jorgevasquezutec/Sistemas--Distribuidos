ce544de6f614c45c87102ee38be81bc74b7f24b5
jorgevasquezutec <jorge.vasquez@utec.edu.pe>
2023-10-23 23:28:00 -0500

    feat: terminado

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 test:
 	curl http://localhost:8881/v1/list?title=trigun
+
+
 jmeter:
-	cd  script/ && sh ./run_jmeter.sh
+	cd script && sh ./run_jmeter.sh
--- script/run_jmeter.sh
+++ script/run_jmeter.sh
@@ -3,7 +3,6 @@
 # Function to run JMeter tests
 run_jmeter_test() {
   local num_requests=$1
-  # local iterations=$2
   local jmx_file=$2
 
   echo "Running JMeter tests with $num_requests requests"
@@ -16,16 +15,5 @@ run_jmeter_test() {
   echo "JMeter tests with $num_requests requests completed."
 }
 
-# # Check if the number of arguments is correct
-# if [ "$#" -ne 1 ]; then
-#   echo "Usage: $0 <num_requests>"
-#   exit 1
-# fi
 
-# # The first command-line argument is the number of requests
-# num_requests=$1
-
-# Run JMeter tests with different configurations using $num_requests
-run_jmeter_test 10 test.jmx
-# run_jmeter_test $num_requests 20 test.jmx
-# run_jmeter_test $num_requests 30 test.jmx
+run_jmeter_test 2 test.jmx
